3116469033263f5024c9fb7f82060093285612f9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-12 17:15:18 -0300

    feat(database): create Sequelize models

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,64 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define(
+    "Sale",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        onDelete: "CASCADE",
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      sellerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        onDelete: "CASCADE",
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      totalPrice: DataTypes.DECIMAL(9, 2),
+      deliveryAddress: DataTypes.STRING,
+      deliveryNumber: DataTypes.STRING,
+      saleDate: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["PEDENTE", "PREPARANDO", "EM TRANSITO", "ENTREGUE"],
+      },
+    },
+    {
+      timestamps: false,
+      tableName: "sales",
+      underscored: true,
+    }
+  );
+
+  Sale.associate = ({ User }) => {
+    Sale.belongsTo(User, {
+      foreignKey: "userId",
+      otherKey: "id",
+      as: "costumer",
+    });
+
+    Sale.belongsTo(User, {
+      foreignKey: "sellerId",
+      otherKey: "id",
+      as: "seller",
+    });
+  };
+
+  return Sale;
+};
