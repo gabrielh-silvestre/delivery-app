6c3737474761f4db6d61550585245a99c14d2a01
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-13 16:21:56 -0300

    fix(api/app): correct cors
    - open cors to all addresses

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(express.json());
-app.use(cors());
+app.use(cors('*'));
 
 app.use('/user', userRouter);
 
