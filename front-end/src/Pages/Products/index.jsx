f2bfcdcdf0f2a430f3fe651b9c9e9c525844a302
Andre Luiz <andreluizgyn1@gmail.com>
2022-07-13 20:25:39 -0300

    feat(pages): Add page structure

--- front-end/src/Pages/Products/index.jsx
+++ front-end/src/Pages/Products/index.jsx
@@ -20,14 +20,34 @@ function Products() {
       <main>
         {products.map(({ id, name, image, price }) => (
           <div key={ id }>
-            <span>{ price }</span>
-            <img src={ image } alt={ name } />
 
-            <span>{ name }</span>
+            <span data-testid={ `customer_products__element-card-price-${id}` }>
+              { price }
+            </span>
 
-            <button type="button">-</button>
-            <span>{ price }</span>
-            <button type="button">+</button>
+            <img
+              data-testid={ `customer_products__img-card-bg-image-${id}` }
+              src={ image }
+              alt={ name }
+            />
+
+            <span data-testid={ `customer_products__element-card-title-${id}` }>
+              { name }
+            </span>
+
+            <button
+              data-testid={ `customer_products__button-card-rm-item-${id}` }
+              type="button"
+            >
+              -
+            </button>
+            <span data-testid={ `customer_products__input-card-quantity-${id}` }>0</span>
+            <button
+              data-testid={ `customer_products__button-card-add-item-${id}` }
+              type="button"
+            >
+              +
+            </button>
           </div>
         ))}
       </main>
