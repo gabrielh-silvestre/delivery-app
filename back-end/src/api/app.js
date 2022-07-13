67aa2d379f1f6bdf17dcc258cb49b55e91beef99
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-13 17:58:35 -0300

    feat: create route for sales
    - apply token middleware validator
    - implement get all endpoint, without seller option yet

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('express-async-errors');
 
-const { userRouter } = require('../shared/http/routes');
+const { userRouter, saleRouter } = require('../shared/http/routes');
 const { errorHandler } = require('../shared/http/middleware/ErrorHandler');
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors('*'));
 
 app.use('/user', userRouter);
+app.use('/sales', saleRouter);
 
 app.use(errorHandler);
 
---
+++ back-end/src/shared/http/routes/sale.router.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const SaleController = require('../../../modules/sale/controller');
+
+const { authHandler } = require('../middleware/Validators/token.validator');
+
+const saleRouter = Router();
+
+saleRouter.get('/', authHandler, SaleController.findAll);
+
+module.exports = { saleRouter };
