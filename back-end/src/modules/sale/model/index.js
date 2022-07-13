df9cc9b8e68e93d93f7f9f1eb9c33b259eb981e2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-13 18:53:40 -0300

    feat(sale/model): implement find sale by id for costumer and seller

--- back-end/src/modules/sale/model/index.js
+++ back-end/src/modules/sale/model/index.js
@@ -1,7 +1,8 @@
-const { Sale } = require('../../../database/models');
+const { Sale, Product, User } = require('../../../database/models');
 
 const {
   sellerSaleReturnNormalizer,
+  detailedSaleFormatter,
 } = require('../../../shared/utils/normalizer');
 
 const findAllByCostumer = async (costumerId) => {
@@ -24,7 +25,48 @@ const findAllBySeller = async (sellerId) => {
   return sellerSaleReturnNormalizer(sales);
 };
 
+const findByIdByCostumer = async (costumerId, saleId) => {
+  const sale = await Sale.findOne({
+    where: { id: saleId, userId: costumerId },
+    include: [
+      {
+        model: User,
+        as: 'seller',
+        attributes: ['id', 'name'],
+      },
+      {
+        model: Product,
+        as: 'products',
+        through: { attributes: ['quantity'], as: 'saleProduct' },
+        attributes: { exclude: ['urlImage'] },
+      },
+    ],
+    attributes: ['id', 'totalPrice', 'saleDate', 'status'],
+  });
+
+  return sale ? detailedSaleFormatter(sale) : null;
+};
+
+const findByIdBySeller = async (sellerId, saleId) => {
+  const sale = await Sale.findOne({
+    where: { id: saleId, sellerId },
+    include: [
+      {
+        model: Product,
+        as: 'products',
+        through: { attributes: ['quantity'], as: 'saleProduct' },
+        attributes: { exclude: ['urlImage'] },
+      },
+    ],
+    attributes: ['id', 'totalPrice', 'saleDate', 'status'],
+  });
+
+  return sale ? detailedSaleFormatter(sale) : null;
+};
+
 module.exports = {
   findAllByCostumer,
   findAllBySeller,
+  findByIdByCostumer,
+  findByIdBySeller,
 };
