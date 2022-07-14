1c36572652e9fdd37d96507529731bee70fd985f
HiagoBriano <hiago.artist@hotmail.com>
2022-07-14 19:09:02 -0300

    feat: checkout page created
    - table component created
    - total calculation of values

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Provider from './Context/Provider';
+import Checkout from './Pages/Checkout';
 import Login from './Pages/Login';
 import Products from './Pages/Products';
 import Register from './Pages/Register';
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/customer/checkout" component={ Checkout } />
         <Route exact path="/">
           <Redirect exact from="/" to="/login" />
         </Route>
