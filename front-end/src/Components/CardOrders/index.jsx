012241d01958e83bf3af1ec6353626f01309afcd
HiagoBriano <hiago.artist@hotmail.com>
2022-07-19 14:31:19 -0300

    feat: created seller order page

--- front-end/src/Components/CardOrders/index.jsx
+++ front-end/src/Components/CardOrders/index.jsx
@@ -3,40 +3,72 @@ import { useHistory } from 'react-router-dom';
 import React from 'react';
 // import './cardOrders.css';
 
-function CardOrders({ order }) {
+function CardOrders({ order, user }) {
   const history = useHistory();
 
-  const handleChangeDatails = () => {
-    history.push(`/customer/orders/${order.id}`);
-  };
-
-  return (
+  return user === 'seller' ? (
     <div
-      onClick={ handleChangeDatails }
-      onKeyPress={ handleChangeDatails }
+      onClick={ () => history.push(`/seller/orders/${order.id}`) }
+      onKeyPress={ () => history.push(`/seller/orders/${order.id}`) }
       role="button"
       tabIndex={ order.id }
     >
       <div>
-        <p>Pedido</p>
-        <h4 data-testid={ `customer_orders__element-order-id-${order.id}` }>
-          {order.id}
-        </h4>
+        <div>
+          <p>Pedido</p>
+          <h4 data-testid={ `seller_orders__element-order-id-${order.id}` }>
+            {order.id}
+          </h4>
+        </div>
+        <div>
+          <h3
+            data-testid={ `seller_orders__element-delivery-status-${order.id}` }
+          >
+            {order.status}
+          </h3>
+        </div>
+        <div>
+          <h4 data-testid={ `seller_orders__element-order-date-${order.id}` }>
+            {new Intl.DateTimeFormat('pt-br').format(new Date(order.saleDate))}
+          </h4>
+          <h4 data-testid={ `seller_orders__element-card-price-${order.id}` }>
+            {Number(order.totalPrice).toFixed(2).replace('.', ',')}
+          </h4>
+        </div>
       </div>
+      <p data-testid={ `seller_orders__element-card-address-${order.id}` }>
+        {`${order.address.street}, ${order.address.number}`}
+      </p>
+    </div>
+  ) : (
+    <div
+      onClick={ () => history.push(`/customer/orders/${order.id}`) }
+      onKeyPress={ () => history.push(`/customer/orders/${order.id}`) }
+      role="button"
+      tabIndex={ order.id }
+    >
       <div>
-        <h3
-          data-testid={ `customer_orders__element-delivery-status-${order.id}` }
-        >
-          {order.status}
-        </h3>
-      </div>
-      <div>
-        <h4 data-testid={ `customer_orders__element-order-date-${order.id}` }>
-          {new Intl.DateTimeFormat('pt-br').format(new Date(order.saleDate))}
-        </h4>
-        <h4 data-testid={ `customer_orders__element-card-price-${order.id}` }>
-          {Number(order.totalPrice).toFixed(2).replace('.', ',')}
-        </h4>
+        <div>
+          <p>Pedido</p>
+          <h4 data-testid={ `customer_orders__element-order-id-${order.id}` }>
+            {order.id}
+          </h4>
+        </div>
+        <div>
+          <h3
+            data-testid={ `customer_orders__element-delivery-status-${order.id}` }
+          >
+            {order.status}
+          </h3>
+        </div>
+        <div>
+          <h4 data-testid={ `customer_orders__element-order-date-${order.id}` }>
+            {new Intl.DateTimeFormat('pt-br').format(new Date(order.saleDate))}
+          </h4>
+          <h4 data-testid={ `customer_orders__element-card-price-${order.id}` }>
+            {Number(order.totalPrice).toFixed(2).replace('.', ',')}
+          </h4>
+        </div>
       </div>
     </div>
   );
@@ -48,7 +80,12 @@ CardOrders.propTypes = {
     saleDate: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
     totalPrice: PropTypes.string.isRequired,
+    address: PropTypes.shape({
+      street: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
   }).isRequired,
+  user: PropTypes.string.isRequired,
 };
 
 export default CardOrders;
