60f5adacde17fd5c3837242bb78f3aa623875870
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-20 17:04:06 -0300

    docs: create docs with swagger
    - implement endpoint /api/docs for swagger

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+
 require('express-async-errors');
 
-const { userRouter, productsRouter, saleRouter } = require('../shared/http/routes');
+const {
+  userRouter,
+  productsRouter,
+  saleRouter,
+} = require('../shared/http/routes');
 const { errorHandler } = require('../shared/http/middleware/ErrorHandler');
+
+const swaggerFile = require('../swagger.json');
 
 const app = express();
 
 // app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(express.json());
 app.use(cors('*'));
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/products', productsRouter);
 app.use('/user', userRouter);
